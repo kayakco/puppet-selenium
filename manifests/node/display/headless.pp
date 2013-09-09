0becfeee682809cb186cdf9a6af04f6ca37a12fa
chqr <choover@kayak.com>
2013-09-09 13:46:39 -0400

    Fix typo affecting use of vnc_password variable for headless displays

--- manifests/node/display/headless.pp
+++ manifests/node/display/headless.pp
@@ -4,7 +4,7 @@ class selenium::node::display::headless(
   $height           = 1024,
   $color            = '24+32',
   $enable_vnc       = $selenium::node::display::enable_vnc,
-  $vnc_password     = $selenium::node::dislpay::vnc_password,
+  $vnc_password     = $selenium::node::display::vnc_password,
   $use_vnc_password = $selenium::node::display::use_vnc_password,
   $vnc_view_only    = $selenium::node::display::vnc_view_only,
 ){
