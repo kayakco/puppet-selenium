351ac5e370f6711bd3391b95b4e72fe9cd5820ae
chqr <choover@kayak.com>
2014-07-01 11:31:27 -0400

    Make it possible to supply custom selenium jar, instead of downloading from google

--- manifests/common/jar.pp
+++ manifests/common/jar.pp
@@ -1,29 +1,45 @@
 # Installs selenium server standalone jar
 class selenium::common::jar(
   $md5sum = undef,
+  $source = undef,
 ){
+
   include selenium::conf
 
   $version  = $conf::version
   $filename = "selenium-server-standalone-${version}.jar"
-  $host     = 'http://selenium-release.storage.googleapis.com'
+  $path     = "${conf::install_dir}/${filename}"
 
-  $templ = '<%= @version.split(".").first(2) * "." %>'
-  $major_minor_version = inline_template($templ)
-
-  $jar_url = "${host}/${major_minor_version}/${filename}"
-  $path    = "${conf::install_dir}/${filename}"
-
-  r9util::download { $jar_url:
-    path    => $path,
-    md5sum  => $md5sum,
-    require => File[$conf::install_dir]
-  }
-  ->
   file { $path:
     owner => $conf::user_name,
     group => $conf::user_group,
     mode  => '0644',
+  }
+
+  if $source == undef {
+    $host     = 'http://selenium-release.storage.googleapis.com'
+
+    $templ = '<%= @version.split(".").first(2) * "." %>'
+    $major_minor_version = inline_template($templ)
+
+    $jar_url = "${host}/${major_minor_version}/${filename}"
+
+    r9util::download { $jar_url:
+      path    => $path,
+      md5sum  => $md5sum,
+      require => File[$conf::install_dir],
+      before  => File[$path],
+    }
+
+  } else {
+
+    notify { 'selenium-version-warning':
+      message => "Using ${source} for ${filename}, version could be wrong",
+    }
+
+    File[$path] {
+      source => $source,
+    }
   }
 
 }
--- spec/classes/common/jar_spec.rb
+++ spec/classes/common/jar_spec.rb
@@ -1,27 +1,60 @@
 require 'spec_helper'
 
 describe 'selenium::common::jar' do
+
   let(:pre_condition){
     "
 class selenium::conf {
-  $version = '31.4.3'
+  $version     = '31.4.3'
   $install_dir = '/mytestdir'
-  $user_name = 'u'
-  $user_group = 'g'
+  $user_name   = 'u'
+  $user_group  = 'g'
 }
 "
   }
+
   let(:facts) do { :r9util_download_curl_version => '1' } end
 
-  it do
-    should include_class('selenium::conf')
-    url = 'http://selenium-release.storage.googleapis.com/31.4/selenium-server-standalone-31.4.3.jar'
-    file = '/mytestdir/selenium-server-standalone-31.4.3.jar'
-    should contain_r9util__download(url).with_path(file)
-    should contain_file(file).with({
-      :owner => 'u',
-      :group => 'g',
-      :mode  => '0644',
-    })
+  let(:url) do
+    'http://selenium-release.storage.googleapis.com/' << 
+    '31.4/selenium-server-standalone-31.4.3.jar'
+  end
+
+  let(:file) do '/mytestdir/selenium-server-standalone-31.4.3.jar' end
+
+  context 'with defaults' do
+    it do
+      should include_class('selenium::conf')
+      should contain_r9util__download(url).with({
+        :path => file,
+      })
+      should contain_file(file).with({
+        :owner => 'u',
+        :group => 'g',
+        :mode  => '0644',
+      })
+    end
+  end
+
+  context 'with md5sum' do
+    let :params do { :md5sum => 'abc' } end
+    it do
+      should contain_r9util__download(url).with({
+        :path   => file,
+        :md5sum => 'abc',
+      })
+    end
+  end
+
+  context 'with source parameter' do
+    let :params do { :source => 'puppet:///myselenium.jar' } end
+
+    it do
+      should_not contain_r9util__download(url)
+      should contain_file(file).with({
+        :path   => file,
+        :source => 'puppet:///myselenium.jar'
+      })
+    end
   end
 end
