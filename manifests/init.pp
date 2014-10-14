50763ba63732f6c422f8d37041cbfdd25c075789
chqr <choover@kayak.com>
2014-10-14 17:47:57 -0400

    Do not start up node service on hubs
    The main selenium class works as a kind of node classifier. If the
    current node is the hub (determined by comparing $::fqdn with the
    $selenium::hub_host parameter), a hub service is started. Previously,
    a node service was always started automatically, whether this host was
    determined to be the hub or not. This behavior is actually undesirable
    in most cases -- for the sake of isolation & stability, you probably
    want the hub to run by itself without any browser processes spawning
    on the same machine and sporadically affecting load. So I have changed
    the default behavior to start up EITHER a hub OR a node, but not both.
    
    To retain previous behavior (where a node service is started on hub,
    too), you can modify your roles / node definitions so that
    
      include selenium
    
    becomes
    
      include selenium
      include selenium::node

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,9 +2,9 @@ class selenium(
   $hub_host,
 ){
 
-  include selenium::node
-
-  if member([$::fqdn,$::hostname],$hub_host){
+  if member([$::fqdn, $::hostname], $hub_host){
     include selenium::hub
+  } else {
+    include selenium::node
   }
 }
--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -23,16 +23,16 @@ describe 'selenium' do
   context 'hostname matches hub setting' do
     let :params do { :hub_host => 'derp' } end
     it do
-      should contain_class('selenium::node')
       should contain_class('selenium::hub')
+      should_not contain_class('selenium::node')
     end
   end
 
   context 'fqdn matches hub setting' do
     let :params do { :hub_host => 'derp.com' } end
     it do
-      should contain_class('selenium::node')
       should contain_class('selenium::hub')
+      should_not contain_class('selenium::node')
     end
   end
 end
